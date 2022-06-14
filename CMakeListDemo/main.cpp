4a16251f54897c1bbef95b965d50d315428be523
chenjl <chenjl@augurit.com>
2022-06-14 10:55:55 +0800

    init CMAKELIST DEMO

---
+++ CMakeListDemo/main.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+# include "hello.h"
+
+using namespace std;
+
+int main() {
+	cout << "hello world!" << endl;
+	cout <<"hello hello.h"<<endl;
+	return 0;
+}
---
+++ CMakeListDemo/src/hello.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+
+using namespace std;
+
+void printHello(){
+    cout<<"hello world!"<<endl;
+}
