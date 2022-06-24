b8af86e12251d03a9f0b40d6cb0c352fb320298f
chenjl <chenjl@augurit.com>
2022-06-24 16:00:22 +0800

    更新注释

--- YOLOV5/yolov5_cpp/yolov5_openvino.h
+++ YOLOV5/yolov5_cpp/yolov5_openvino.h
@@ -22,11 +22,8 @@ public:
     } Object;
     YOLOV5();
     ~YOLOV5();
-    //初始化
     bool init(string xml_path, double cof_threshold, double nms_area_threshold);
-    //释放资源
     bool uninit();
-    //处理图像获取结果
     bool process_frame(Mat& inframe, vector<Object>& detected_objects);
 
 private:
@@ -35,14 +32,12 @@ private:
     bool parse_yolov5(const Blob::Ptr& blob, int net_grid, float cof_threshold,
         vector<Rect>& o_rect, vector<float>& o_rect_cof);
     Rect detet2origin(const Rect& dete_rect, float rate_to, int top, int left);
-    //存储初始化获得的可执行网络
     ExecutableNetwork _network;
     OutputsDataMap _outputinfo;
     string _input_name;
-    //参数区
-    string _xml_path;                             //OpenVINO模型xml文件路径
-    double _cof_threshold;                //置信度阈值,计算方法是框置信度乘以物品种类置信度
-    double _nms_area_threshold;  //nms最小重叠面积阈值
+    string _xml_path;                            
+    double _cof_threshold;         
+    double _nms_area_threshold;  
 };
 
 
