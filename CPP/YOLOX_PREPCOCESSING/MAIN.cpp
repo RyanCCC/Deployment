6982f6885708eed05823c23a77df70d24b661c9b
chenjl <chenjl@augurit.com>
2022-07-11 17:20:07 +0800

    init cpp and add yolox preprocessing

---
+++ CPP/YOLOX_PREPCOCESSING/MAIN.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+    //YOLOX 预处理
+    Mat src = imread("../src/20210803173137.jpg");
+    //1. resize
+
+    //2. expand dimension
+
+
+    imshow("Test", src);
+    waitKey(0);
+    destroyAllWindows();
+    return 0;
+}
