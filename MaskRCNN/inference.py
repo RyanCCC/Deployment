e7ce4590361b0dd794d21dc7bffe976fbbfdfd96
chenjl <chenjl@augurit.com>
2022-07-20 16:07:27 +0800

    mask rcnn优化

---
+++ MaskRCNN/inference.py
@@ -0,0 +1,100 @@
+from PIL import Image
+import tensorflow as tf
+import numpy as np
+from utils.anchors import get_anchors
+from utils.utils import mold_inputs,unmold_detections
+from utils import visualize
+from utils.config import Config
+import os
+from glob import glob
+import json
+import base64
+import tf2onnx
+from tqdm import tqdm
+import onnxruntime as ort
+
+tf.compat.v1.enable_eager_execution()
+
+img_pattern  = './samples/*.jpg'
+model_path = './models/maskrcnn_coco'
+class_path = './data/coco_classes.txt'
+
+class intEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.intc):
+            return float(obj)
+    
+        return json.JSONEncoder.default(self, obj)
+
+def get_class(classes_path):
+        classes_path = os.path.expanduser(classes_path)
+        with open(classes_path) as f:
+            class_names = f.readlines()
+        class_names = [c.strip() for c in class_names]
+        class_names.insert(0,"BG")
+        return class_names
+
+def img2base64(image_path):
+    with open(image_path, 'rb') as f:
+        image = f.read()
+    image_base64 = str(base64.b64encode(image), encoding='utf-8')
+    return image_base64 
+
+class_names = get_class(class_path)
+def get_config():
+    class InferenceConfig(Config):
+        NUM_CLASSES = len(class_names)
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+        DETECTION_MIN_CONFIDENCE = 0.8
+        NAME = "Customer"
+        RPN_ANCHOR_SCALES =  (16, 32, 64, 128, 256)
+        IMAGE_MIN_DIM = 512
+        IMAGE_MAX_DIM = 512
+        IMAGE_SHAPE =  [512, 512 ,3]
+
+    config = InferenceConfig()
+    return config
+
+InferenceConfig = get_config()
+model = tf.keras.models.load_model(model_path)
+model.summary()
+
+# 将模型导出为onnx
+output_path = os.path.join('./models/mask_rcnn_13.onnx')
+# model_proto, _ = tf2onnx.convert.from_keras(model, opset=13, output_path=output_path)
+# output_names = [n.name for n in model_proto.graph.output]
+# print(output_names)
+
+# load onnx model 
+outputs_names = ['mrcnn_detection', 'mrcnn_class', 'mrcnn_bbox', 'mrcnn_mask', 'ROI', 'rpn_class', 'rpn_bbox']
+onnx_model = ort.InferenceSession(output_path)
+
+
+
+for image_name in tqdm(glob(img_pattern)):
+    image = Image.open(image_name).convert('RGB')
+    imageWidth, imageHeight = image.size
+    image = [np.array(image)]
+    molded_images, image_metas, windows = mold_inputs(InferenceConfig,image)
+    image_shape = molded_images[0].shape
+    anchors = get_anchors(InferenceConfig,image_shape)
+    anchors = np.broadcast_to(anchors, (1,) + anchors.shape)
+    
+    onnx_pred =onnx_model.run(outputs_names, {"input_image":molded_images.astype(np.float32), "input_image_meta":image_metas.astype(np.float32), "input_anchors":anchors.astype(np.float32)})
+    detections, _, _, mrcnn_mask, _, _, _ = onnx_pred
+    # detections, _, _, mrcnn_mask, _, _, _ =model.predict([molded_images, image_metas, anchors], verbose=0)
+    final_rois, final_class_ids, final_scores, final_masks =unmold_detections(detections[0], mrcnn_mask[0],image[0].shape, molded_images[0].shape,windows[0])
+
+    r = {
+        "rois": final_rois,
+        "class_ids": final_class_ids,
+        "scores": final_scores,
+        "masks": final_masks,
+    }
+
+
+    drawed_image, shapes = visualize.display_instances(image[0], r['rois'], r['masks'], r['class_ids'], 
+                                    class_names, r['scores'])
+
+    drawed_image.show()
---
+++ MaskRCNN/utils/visualize.py
@@ -0,0 +1,112 @@
+import os
+import sys
+import random
+import itertools
+import colorsys
+import numpy as np
+
+from skimage.measure import find_contours
+from PIL import Image
+import cv2
+ROOT_DIR = os.path.abspath("../")
+
+sys.path.append(ROOT_DIR)
+
+#---------------------------------------------------------#
+#  Visualization
+#---------------------------------------------------------#
+def random_colors(N, bright=True):
+    """
+    生成随机颜色
+    """
+    brightness = 1.0 if bright else 0.7
+    hsv = [(i / N, 1, brightness) for i in range(N)]
+    colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
+    return colors
+
+
+def apply_mask(image, mask, color, alpha=0.5):
+    """
+    打上mask图标
+    """
+    for c in range(3):
+        image[:, :, c] = np.where(mask == 1,
+                                  image[:, :, c] *
+                                  (1 - alpha) + alpha * color[c] * 255,
+                                  image[:, :, c])
+    return image
+
+
+def display_instances(image, boxes, masks, class_ids, class_names,
+                      scores=None, title="",
+                      figsize=(16, 16), 
+                      show_mask=True, show_bbox=False,
+                      colors=None, captions=None):
+    # instance的数量
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances to display *** \n")
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
+    colors = colors or random_colors(N)
+
+    # 当masked_image为原图时是在原图上绘制
+    # 如果不想在原图上绘制，可以把masked_image设置成等大小的全0矩阵
+    masked_image = np.array(image,np.uint8)
+    shapes = []
+    for i in range(N):
+        color = colors[i]
+
+        # 该部分用于显示bbox
+        if not np.any(boxes[i]):
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        if show_bbox:
+            cv2.rectangle(masked_image, (x1, y1), (x2, y2), (color[0] * 255,color[1] * 255,color[2] * 255), 2)
+
+        # 该部分用于显示文字与置信度
+        if not captions:
+            class_id = class_ids[i]
+            score = scores[i] if scores is not None else None
+            label = class_names[class_id]
+            caption = "{} {:.3f}".format(label, score) if score else label
+        else:
+            caption = captions[i]
+        
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(masked_image, caption, (x1, y1 + 8), font, 1, (255, 255, 255), 2)
+
+        # 该部分用于显示语义分割part
+        mask = masks[:, :, i]
+        if show_mask:
+            masked_image = apply_mask(masked_image, mask, color)
+
+        # 画出语义分割的范围
+        padded_mask = np.zeros(
+            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+
+        # 画出语义分割边界框
+        '''
+        参数说明:
+        mode: cv2.RETE_EXTERNAL  仅检索极端的外部轮廓，为所有轮廓设置了层次hierarchy[i][2]= hierarchy[i][3]=-1
+              cv2.RETR_LIST 在不建立任何层次关系的情况下检索所有轮廓
+              cv2.RETR_CCOMP 检索所有轮廓并将其组织为两级层次结构。在顶层，组件具有外部边界；在第二层，有孔的边界。如果所连接零部件的孔内还有其他轮廓，则该轮廓仍将放置在顶层。
+              cv2.RETR_TREE 检索所有轮廓，并重建嵌套轮廓的完整层次
+              cv2.RETE_FLOODFILL 输入图像也可以是32位的整形图像 CV——32SC1
+        method: cv2.CHAIN_APPROX_NONE 存储所有的轮廓点，任何一个包含一两个点的子序列（不改变顺序索引的连续的）相邻
+                cv2.CHAIN_APPROX_SIMPLE  压缩水平，垂直和对角线段，仅保留其端点。 例如，一个直立的矩形轮廓编码有4个点
+                cv2.CHAIN_APPROX_TC89_L1 和 cv2.CHAIN_APPROX_TC89_KCOS 近似算法
+        '''
+        contours, h= cv2.findContours(padded_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # cv2.polylines(masked_image, contours, 1, (color[0] * 255,color[1] * 255,color[2] * 255), 2)
+
+        # 根据轮廓线画出近似轮廓
+        epsilon = 0.01*cv2.arcLength(contours[0], True)
+        approx = cv2.approxPolyDP(contours[0], epsilon, True)
+        cv2.polylines(masked_image, [approx], 1, (color[0] * 255,color[1] * 255,color[2] * 255), 2)
+        shapes.append((label, [approx]))
+            
+
+    img = Image.fromarray(np.uint8(masked_image))
+    return img, shapes
